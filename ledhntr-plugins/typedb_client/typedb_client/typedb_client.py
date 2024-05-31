fdce34616a08ab97c178b1d2aef9ca5d77b5d7c8
TheDr1ver <driver.nick@gmail.com>
2024-05-31 16:50:19 -0400

    revert extra get

--- ledhntr-plugins/typedb_client/typedb_client/typedb_client.py
+++ ledhntr-plugins/typedb_client/typedb_client/typedb_client.py
@@ -2591,11 +2591,17 @@ class TypeDBClient(ConnectorPlugin):
                         if attr.label == thing.keyattr:
                             # returns an explicit thing based on keyattr value
                             fmt_val = self.format_value_query(attr.value)
-                            tql = (
-                                f' ${thing.label}_{thing.counter} isa {thing.label},'
-                                f' has {attr.label} ${attr.label}_{thing.counter};'
-                                f' ${attr.label}_{thing.counter} = {fmt_val};'
-                            )
+                            if fmt_val:
+                                tql = (
+                                    f' ${thing.label}_{thing.counter} isa {thing.label},'
+                                    f' has {attr.label} ${attr.label}_{thing.counter};'
+                                    f' ${attr.label}_{thing.counter} = {fmt_val};'
+                                )
+                            else:
+                                tql = (
+                                    f' ${thing.label}_{thing.counter} isa {thing.label},'
+                                    f' has {attr.label} ${attr.label}_{thing.counter};'
+                                )
                             if limit_get:
                                 tql += f" get ${thing.label}_{thing.counter};"
             if not tql:
@@ -2673,7 +2679,7 @@ class TypeDBClient(ConnectorPlugin):
         else:
             tql = f' ${thing.label}_{thing.counter} isa {thing.label}'
         if not hasattr(thing, 'has'):
-            tql += "; get;"
+            tql += ";"
             if hasattr(thing, 'value') and thing.value is not None:
                 fmt_val = self.format_value_query(thing.value)
                 tql += f' ${thing.label}_{thing.counter}={fmt_val}; get;'
