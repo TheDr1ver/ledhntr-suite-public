2c24967665fcff4f7edbd204a6fa3a24d3917f33
Nick Driver <driver.nick@gmail.com>
2024-08-13 21:12:29 +0000

    move message builder to separate heading

--- ledhntr-plugins/slack_client/slack_client/modal_builder/modal_builder.py
+++ ledhntr-plugins/slack_client/slack_client/modal_builder/modal_builder.py
@@ -732,6 +732,77 @@ class ModalBuilder():
         return input
 
     #&##########################################################################
+    #& Message Layouts
+    #&##########################################################################
+
+    #~ New Hits
+    @staticmethod
+    async def new_hits(
+        data: Dict = {},
+        interesting_things: List = None,
+        con_list: List = None,
+    )->Dict:
+        blocks = []
+        db = next(iter(data))
+        new_stuff = data[db]
+        #; Format links for quick context lookups
+        link_formats = await get_link_formats()
+
+        #; Check for interesting things first:
+        #; If there's nothing interesting, return an empty block
+        if not any(thing_type in interesting_things for thing_type in new_stuff):
+            return []
+
+        blocks.append(await ModalBuilder.header_block(f":collision: NEW HITS [{db}]"))
+        blocks.append(await ModalBuilder.divider_block())
+        blocks.append(
+            await context_block(elements=[('mrkdwn', await get_date())])
+        )
+        for thing_type, things in new_stuff.items():
+            if thing_type.lower() not in interesting_things:
+                ModalBuilder._log.debug(f"{thing_type} is not interesting. Skipping.")
+                continue
+            blocks.append(await basic_rich_text(text=thing_type.upper(), bold=True))
+            thing_added = False
+            for thing in things:
+                keyval = next(iter(thing))
+                #! DEBUG - this should never happen normally
+                if 'confidence' not in thing[keyval]:
+                    confidence = 0
+                else:
+                    confidence = thing[keyval]['confidence'][0]
+                if con_list and confidence not in con_list:
+                    continue
+                iid = thing[keyval]['iid']
+                lines = [
+                    f"`{keyval}`"
+                ]
+                if thing_type.lower() in link_formats:
+                    links = ""
+                    for text, link in link_formats[thing_type.lower()].items():
+                        links += f"<{link.format(value=keyval)}|{text}> | "
+                    links = links.rstrip(" | ")
+                    lines.append(links)
+                mrkdwn = "\n".join(lines)
+                button = await button_block(
+                    text=await get_con_format(int(confidence)),
+                    button_text=mrkdwn,
+                    value=f"{db}|{iid}",
+                    action_id='set_confidence_modal',
+                    verbatim=True,
+                )
+                blocks.append(button)
+                thing_added = True
+            #; If we didn't add anything, remove the heading.
+            if not thing_added:
+                blocks.pop()
+        #; This means all we have is the DB header, divider, and context date
+        if len(blocks) == 3:
+            return []
+        return blocks
+
+
+    #&##########################################################################
     #& Modal Layouts
     #&##########################################################################
     #~ Simple Popup
@@ -817,74 +888,9 @@ class ModalBuilder():
         )
         return modal
 
-
-    #~ New Hits
-    @staticmethod
-    async def new_hits(
-        data: Dict = {},
-        interesting_things: List = None,
-        con_list: List = None,
-    )->Dict:
-        blocks = []
-        db = next(iter(data))
-        new_stuff = data[db]
-        #; Format links for quick context lookups
-        link_formats = await get_link_formats()
-
-        #; Check for interesting things first:
-        #; If there's nothing interesting, return an empty block
-        if not any(thing_type in interesting_things for thing_type in new_stuff):
-            return []
-
-        blocks.append(await ModalBuilder.header_block(f":collision: NEW HITS [{db}]"))
-        blocks.append(await ModalBuilder.divider_block())
-        blocks.append(
-            await context_block(elements=[('mrkdwn', await get_date())])
-        )
-        for thing_type, things in new_stuff.items():
-            if thing_type.lower() not in interesting_things:
-                ModalBuilder._log.debug(f"{thing_type} is not interesting. Skipping.")
-                continue
-            blocks.append(await basic_rich_text(text=thing_type.upper(), bold=True))
-            thing_added = False
-            for thing in things:
-                keyval = next(iter(thing))
-                #! DEBUG - this should never happen normally
-                if 'confidence' not in thing[keyval]:
-                    confidence = 0
-                else:
-                    confidence = thing[keyval]['confidence'][0]
-                if con_list and confidence not in con_list:
-                    continue
-                iid = thing[keyval]['iid']
-                lines = [
-                    f"`{keyval}`"
-                ]
-                if thing_type.lower() in link_formats:
-                    links = ""
-                    for text, link in link_formats[thing_type.lower()].items():
-                        links += f"<{link.format(value=keyval)}|{text}> | "
-                    links = links.rstrip(" | ")
-                    lines.append(links)
-                mrkdwn = "\n".join(lines)
-                button = await button_block(
-                    text=await get_con_format(int(confidence)),
-                    button_text=mrkdwn,
-                    value=f"{db}|{iid}",
-                    action_id='set_confidence_modal',
-                    verbatim=True,
-                )
-                blocks.append(button)
-                thing_added = True
-            #; If we didn't add anything, remove the heading.
-            if not thing_added:
-                blocks.pop()
-        #; This means all we have is the DB header, divider, and context date
-        if len(blocks) == 3:
-            return []
-        return blocks
-
     #~ Add Thing Modal
+    #. First we need to break out the mojo CMD and the args into individual params
+    #. Then we can worry about converting the rest of the function.
 
     #~ Edit Thing Modal
 
